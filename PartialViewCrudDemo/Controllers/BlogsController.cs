9308c3a79ce07ff1551a46ba0ec79e120ccd8eed
David Santafe <santafedavid01@gmail.com>
2021-11-03 23:18:30 -0500

    Agregar archivos de proyecto.

---
+++ PartialViewCrudDemo/Controllers/BlogsController.cs
@@ -0,0 +1,123 @@
+﻿using PartialViewCrudDemo.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace PartialViewCrudDemo.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private DBQuickstartEntities db = new DBQuickstartEntities();
+
+        // GET: Blogs
+        public ActionResult Index()
+        {
+            return View(db.Blog.ToList());
+        }
+
+        // GET: Blogs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blog.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Create
+        public ActionResult Create()
+        {
+            return PartialView(new Blog());
+        }
+
+        // POST: Blogs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blog.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return PartialView(blog);
+        }
+
+        // GET: Blogs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blog.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(blog);
+        }
+
+        // POST: Blogs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return PartialView(blog);
+        }
+
+        // GET: Blogs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blog.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blog blog = db.Blog.Find(id);
+            db.Blog.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
